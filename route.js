75c80f7041e10ebc8873c3c51d574fe26d7a935f
thabatacristini <thabatacristini@gmail.com>
2024-04-01 22:12:32 -0300

    insere código da API

--- route.js
+++ route.js
@@ -1,1 +1,14 @@
 // Vamos incluir nossas rotas aqui
+
+const { getAll, create } = require('./controller')
+const express = require('express')
+const router  = express.Router()
+
+//vai permitir entregar como resposta de uma requisição a lista de flashcards
+
+
+  router.get('/cards', getAll) 
+
+  router.post('/cards', create) 
+
+  module.exports = router
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require('express')
+const express = require('express')//trazendo funcionalidades do express (framework do node)
 const cors = require('cors')
 
-const app = express()
-const PORTA = 3333
-app.use(cors())
+const app = express() //instanciando o express (segunda etapa de configuração do express) 
+const PORTA = 3333 // criando a constante porta para guardar a informação da porta
+app.use(cors()) //instanciando cors e terminando a configuração para liberar a api ser integrada pelo nosso front
 
-app.use(express.json())
+app.use(express.json()) //dizendo que o tipo de dado que a minha api permite enviar é JSON
 
-app.get('/ola', function(request, response) {
-  response.json({ message: 'Começou o backend'})
-})
+const router = require('./route')
+app.use('/', router)
 
-app.listen(PORTA, console.log(`Rodando servidor na porta: ${PORTA}`))
+app.listen(PORTA, console.log(`Rodando servidor na porta: ${PORTA}`)) //ligando o servidor
